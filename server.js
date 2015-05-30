8bf7154b11469ab56c4bd5351d564846d55e4ef2
Sean Hill <sean.daryl.hill@gmail.com>
2015-05-30 15:14:53 -0700

    Removed morgan logger, unneeded

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 var express = require('express');
-var morgan = require('morgan');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-app.use(morgan(':date[web] - :method :url responded :status in :response-time ms' + (process.env.NODE_ENV === 'production' ? ' - :remote-addr :referrer - :user-agent' : ''), { 
-    immediate: false 
-}));
 
 app.get('/', function(req, res) {
 	res.send('Hello Raspbian!');
